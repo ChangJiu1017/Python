fa2f1cdb5217a2aeb19de7682596629ca4edb06a
ChangJiu1017 <changxinyu001@gmail.com>
2024-02-25 16:50:35 +0800

    init Python

---
+++ ai.py
@@ -0,0 +1,22 @@
+
+from textblob import TextBlob
+
+def analyze_emotion(text):
+    testimonial = TextBlob(text)
+    polarity = testimonial.sentiment.polarity
+    subjectivity = testimonial.sentiment.subjectivity
+
+    if polarity > 0:
+        sentiment = 'positive'
+    elif polarity < 0:
+        sentiment = 'negative'
+    else:
+        sentiment = 'neutral'
+
+    return sentiment, subjectivity
+
+text = input('please enter your text: ')
+# text = "This is a great product."
+sentiment, subjectivity = analyze_emotion(text)
+print(sentiment, subjectivity)
+
